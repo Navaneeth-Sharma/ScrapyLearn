28eec433fa7079279097afd0ff470bc0e3631af8
Navaneeth Sharma <63489382+Navaneeth-Sharma@users.noreply.github.com>
2021-12-15 22:36:04 +0530

    scrapy initials

---
+++ quotetutorial/quotetutorial/spiders/quotes_spider.py
@@ -0,0 +1,12 @@
+import scrapy
+
+class QuoteSpider(scrapy.Spider):
+    name = 'quotes' # Name of the spider to reference in the command line
+    start_urls = [
+        'http://quotes.toscrape.com/', # start_urls is a list of urls to scrape
+    ]
+
+    def parse(self, response):
+        title = response.css('title::text').extract() # extract the title of the page and store it in a list 
+        # here ::text is a css selector that extracts the text from the title tag
+        yield {'titletext': title} # yield is a keyword that sends the data to the pipeline
