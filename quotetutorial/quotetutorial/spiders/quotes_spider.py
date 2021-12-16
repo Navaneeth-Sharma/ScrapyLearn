63ad78c8700e4df2cf612e158eff242edbf60b81
Navaneeth Sharma <63489382+Navaneeth-Sharma@users.noreply.github.com>
2021-12-16 23:37:54 +0530

    Update quotes_spider.py

--- quotetutorial/quotetutorial/spiders/quotes_spider.py
+++ quotetutorial/quotetutorial/spiders/quotes_spider.py
@@ -7,6 +7,10 @@ class QuoteSpider(scrapy.Spider):
     ]
 
     def parse(self, response):
+        # Extracting the quotes from the response 
+        
         title = response.css('title::text').extract() # extract the title of the page and store it in a list 
         # here ::text is a css selector that extracts the text from the title tag
         yield {'titletext': title} # yield is a keyword that sends the data to the pipeline
+
+    
