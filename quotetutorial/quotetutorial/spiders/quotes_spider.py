1c323b1e5c7f72b8a78712307d8ee6341a59588c
Navaneeth Sharma <63489382+Navaneeth-Sharma@users.noreply.github.com>
2021-12-18 23:36:17 +0530

    css selector

--- quotetutorial/quotetutorial/spiders/quotes_spider.py
+++ quotetutorial/quotetutorial/spiders/quotes_spider.py
@@ -1,16 +1,17 @@
 import scrapy
 
+
 class QuoteSpider(scrapy.Spider):
-    name = 'quotes' # Name of the spider to reference in the command line
+    name = 'quotes'  # Name of the spider to reference in the command line
     start_urls = [
-        'http://quotes.toscrape.com/', # start_urls is a list of urls to scrape
+        'http://quotes.toscrape.com/',  # start_urls is a list of urls to scrape
     ]
 
     def parse(self, response):
-        # Extracting the quotes from the response 
-        
-        title = response.css('title::text').extract() # extract the title of the page and store it in a list 
-        # here ::text is a css selector that extracts the text from the title tag
-        yield {'titletext': title} # yield is a keyword that sends the data to the pipeline
 
-    
+        # Extracting the quotes from the response
+        # extract the title of the page and store it in a list
+        # here ::text is a css selector that extracts the text from the title tag
+        title = response.css('title::text').extract()
+        # yield is a keyword that sends the data to the pipeline
+        yield {'titletext': title}
