9dc184f6c06bbdbbc561040320a797662f59b32e
Navaneeth Sharma <63489382+Navaneeth-Sharma@users.noreply.github.com>
2022-02-18 19:16:08 +0530

    Update advanced.py

--- amazon_crawler/amazon_crawler/spiders/advanced.py
+++ amazon_crawler/amazon_crawler/spiders/advanced.py
@@ -4,6 +4,7 @@ from scrapy.linkextractors import LinkExtractor
 from scrapy.spiders import CrawlSpider, Rule
 from ..items import AmazonCrawlerItem
 
+
 class AdvancedSpider(CrawlSpider):
     name = 'advanced'
     allowed_domains = ['amazon.com']
@@ -18,8 +19,6 @@ class AdvancedSpider(CrawlSpider):
     # rules = (
     #     Rule(LinkExtractor(allow=r'Items/'), callback='parse_item', follow=True),
     # )
-
-
 
     def parse_item(self, response):
 
@@ -40,7 +39,7 @@ class AdvancedSpider(CrawlSpider):
                 print(e)
 
             yield item
-            
+
         next_page = "https://www.amazon.com/s?k=gaming+headsets&page="+str(AdvancedSpider.page_number)+"&pd_rd_r=36dc48bd-7fad-49d3-ac92-222fe4fe5be5&pd_rd_w=J7m4j&pd_rd_wg=ZAQQK&pf_rd_p=12129333-2117-4490-9c17-6d31baf0582a&pf_rd_r=ZFX51PCD7D5QWEF90KR9&qid=1644770182&ref=sr_pg_2"
         if AdvancedSpider.page_number <= 20:
             AdvancedSpider.page_number += 1
